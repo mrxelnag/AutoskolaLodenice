69f036c055f875146357a995abbc4ba8f17f2bd4
mrxelnag <93269735+mrxelnag@users.noreply.github.com>
2022-10-10 12:29:39 +0200

    scroll snap bug

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@ const modalOpen = document.querySelector('.modal-open')
 const modalClose = document.querySelector('.modal-close1')
 const modalClose2 = document.querySelector('.modal-close2')
 const modal = document.querySelector('#modal')
+const dialogWrapper = document.querySelector('#dialog-wrapper')
 
 modalOpen.addEventListener('click', (event) => {
     modal.showModal();
+    dialogWrapper.classList.remove('scroll-snap-start');
     // modal.scrollTop = 0;
 })
 modalClose.addEventListener('click', () => {
     modal.close();
+    dialogWrapper.classList.add('scroll-snap-start')
 })
 modalClose2.addEventListener('click', () => {
     modal.close();
+    dialogWrapper.classList.add('scroll-snap-start')
 })
 var lightbox = jQuery('.vehicle-image a').simpleLightbox({ /* options */ });
