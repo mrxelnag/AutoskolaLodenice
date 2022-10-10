ae5bf0695e51877f7726318030650fe3daf1af5a
mrxelnag <93269735+mrxelnag@users.noreply.github.com>
2022-10-10 16:30:47 +0200

    fix dialogu more

--- js/main.js
+++ js/main.js
@@ -6,19 +6,11 @@ const dialogWrapper = document.querySelector('#dialog-wrapper')
 
 modalOpen.addEventListener('click', (event) => {
     modal.showModal();
-
-    setTimeout(function () {
-        $('#dialog-wrapper').removeClass("scroll-snap-start");
-    }, 1000);
-
-    modal.scrollTop = 0;
 })
 modalClose.addEventListener('click', () => {
-    modal.close();
-    dialogWrapper.classList.add('scroll-snap-start')
+    modal.close();  
 })
 modalClose2.addEventListener('click', () => {
-    modal.close();
-    dialogWrapper.classList.add('scroll-snap-start')
+    modal.close();  
 })
 var lightbox = jQuery('.vehicle-image a').simpleLightbox({ /* options */ });
